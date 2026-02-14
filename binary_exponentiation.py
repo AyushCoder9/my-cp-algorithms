c2cdb0e9116a36dc84a3998fd896bec35090f780
AyushCoder9 <ayushsingh.ofc1@gmail.com>
2026-02-15 00:13:11 +0530

    Binary Exponentiation

---
+++ binary_exponentiation.py
@@ -0,0 +1,14 @@
+def power(a, b):
+    result = 1
+    
+    while b > 0:
+        if b % 2 == 1:    
+            result *= a
+        
+        a *= a           
+        b //= 2           
+    
+    return result
+
+print(power(2, 10))
+
