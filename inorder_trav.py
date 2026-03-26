c117de53177df6f3609e3fa3b5952f49f65111f5
AyushCoder9 <ayushsingh.ofc1@gmail.com>
2026-03-27 00:06:13 +0530

    preorder traversal

---
+++ inorder_trav.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def printInorder(root):
+    if root:
+        printInorder(root.left) 
+        print(root.data, end=" ")
+        printInorder(root.right)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Inorder traversal:")
+printInorder(root) 
